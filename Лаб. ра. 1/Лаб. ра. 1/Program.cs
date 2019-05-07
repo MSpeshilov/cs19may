311fbf2b124cd13402a0c064aecdbf7896f070e4
MSpeshilov <slesar1996@gmail.com>
2019-05-07 20:02:57 +0600

    Сделали 1 задачу в 1 лабораторной

---
+++ Лаб. ра. 1/Лаб. ра. 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаб.ра._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+---+");
+            Console.WriteLine("|   |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("+---+");
+            Console.WriteLine("#\n##\n###\n####");
+            Console.WriteLine(" /\\\n//\\\\\n//\\\\\n []");
+            Console.WriteLine("{\"x\" : 15, \"y\": 28.7}");
+
+        }
+    }
+}
