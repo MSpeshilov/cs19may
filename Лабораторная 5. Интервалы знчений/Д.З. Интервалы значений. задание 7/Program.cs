a9079c64b87948dd1c07d77b0e7cbb3fc9d5b4fc
MSpeshilov <slesar19966@gmail.com>
2019-07-03 15:55:15 +0600

    Д.З. Интервалы значений. Задание 7

--- Лабораторная 5. Интервалы знчений/Д.З. Интервалы значений. задание 7/Program.cs
+++ Лабораторная 5. Интервалы знчений/Д.З. Интервалы значений. задание 7/Program.cs
@@ -14,7 +14,25 @@ namespace Д.З.Интервалы_значений.задание_7
             int redb = int.Parse(Console.ReadLine());
             int greena = int.Parse(Console.ReadLine());
             int greenb = int.Parse(Console.ReadLine());
-
+            int rab = redb - reda;
+            int gab = greenb - greena;
+            int sr = rab / 2;
+            int sg = gab / 2;
+            int red = reda + sr;
+            int green = greena + sg;
+            if ((reda > redb) || (greena > greenb))
+            {
+                Console.WriteLine("Некорректный интервал. Левая граница должна быть меньше правой.");
+            }else if (red < green)
+            {
+                Console.WriteLine("Красный слева, зеленый справа");
+            }else if (red == green)
+            {
+                Console.WriteLine("Середины интервалов совпадают");
+            }else
+            {
+                Console.WriteLine("Зеленый слева, красный справа");
+            }
         }
     }
 }
