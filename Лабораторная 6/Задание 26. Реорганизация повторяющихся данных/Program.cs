ffa1366fd67757a1d6685f353b1bfc48294c4a6f
q <a@a>
2019-07-17 20:35:12 +0600

    kf, 6

---
+++ Лабораторная 6/Задание 26. Реорганизация повторяющихся данных/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_26.Реорганизация_повторяющихся_данных
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = int.Parse(Console.ReadLine());
+            int w =  y + 1;
+
+            int q = 1;
+            int a = 1;
+            if (y < 1 || y > 15)
+            {
+                Console.Write("Значение Y должно быть в интервале [0, 15]");
+            }
+            else
+            {
+                while (q <= y)
+                {
+                    Console.Write("#");
+                    q = q + 1;
+                    a = a + 1;
+                    if (a == w)
+                    {
+                        Console.Write(".");
+                        a = 1;
+                        
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Лабораторная 6/Реорганизация повторяющихся данных. Задание 19/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Реорганизация_повторяющихся_данных.Задание_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n < 0)
+            {
+                Console.Write("Значение N должно быть неотрицательным");
+            }
+            else
+            {
+                int a = 0;
+                int q= 0;
+                
+                while (q <= n)
+                {
+                    Console.WriteLine("{0} - {1}",q,a);
+                    q = q + 1;
+                    a = a + 1;
+                    if (a == 4)
+                    {
+                        a = 0;
+                    }
+                }/*
+                Console.WriteLine("{0} - {1}",q,a);
+                q = q + 1;
+                a = a + 1;
+                Console.WriteLine("{0} - {1}", q, a);
+                q = q + 1;
+                a = a + 1;
+                Console.WriteLine("{0} - {1}", q, a);
+                q = q + 1;
+                a = a + 1;
+                Console.WriteLine("{0} - {1}", q, a);
+                q = q + 1;
+                a = a + 1;
+                a = 0;
+                Console.WriteLine("{0} - {1}", q, a);
+                q = q + 1;
+                a = a + 1;
+                Console.WriteLine("{0} - {1}", q, a);
+                q = q + 1;
+                a = a + 1;
+                Console.WriteLine("{0} - {1}", q, a);
+                q = q + 1;
+                a = a + 1;
+                
+                Console.WriteLine("{0} - {1}", q, a);
+                q = q + 1;
+                a = a + 1;
+                a = 0;
+                Console.WriteLine("{0} - {1}",q,a);
+                q = q + 1;
+                a = a + 1;
+                Console.WriteLine("{0} - {1}", q, a);
+                q = q + 1;
+                a = a + 1;
+                Console.WriteLine("{0} - {1}", q, a);
+                q = q + 1;
+                a = a + 1;
+                Console.WriteLine("{0} - {1}", q, a);
+                q = q + 1;
+                a = a + 1;
+                a = 0;
+                Console.WriteLine("{0} - {1}", q, a);
+                q = q + 1;
+                a = a + 1;*/
+            }
+        }
+    }
+}
