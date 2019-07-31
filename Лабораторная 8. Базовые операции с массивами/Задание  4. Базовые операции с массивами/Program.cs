0362f7f3aa6024976b4338ef45014311766c6c2b
MSpeshilov <slesar19966@gmail.com>
2019-07-31 20:30:02 +0600

    лаб 8

---
+++ Лабораторная 8. Базовые операции с массивами/Задание  4. Базовые операции с массивами/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание__4.Базовые_операции_с_массивами
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String data = Console.ReadLine();
+            String[] q;
+            int a = 0;
+            q = data.Split(' ');
+            while (a < q.Length)
+            {
+                Console.WriteLine(q[a]+ " " + a);
+                    a++;
+                
+            }
+            /*Console.WriteLine(q[a] + " " + a);
+            Console.WriteLine(q[a] + " " + a);
+            Console.WriteLine(q[a] + " " + a);*/
+        }
+    }
+}
---
+++ Лабораторная 8. Базовые операции с массивами/Лабораторная 8. Базовые операции с массивами/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторная_8.Базовые_операции_с_массивами
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            String data = Console.ReadLine();
+            String[] q;
+            q = data.Split(' ');
+            if (String.IsNullOrEmpty(data))
+            {
+                Console.Write("Исходная строка пуста");
+                return;
+            }
+            if (a < 0|| a > q.Length)
+            {
+                Console.Write("Число A должно быть в интервале [0, размер массива)");
+                return;
+            }
+           
+            Console.Write(q[a]);
+            
+        }
+    }
+}
