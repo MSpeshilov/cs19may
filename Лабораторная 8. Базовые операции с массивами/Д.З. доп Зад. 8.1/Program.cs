228f38f71352e333a028278f3dcc10a232d4b513
MSpeshilov <slesar19966@gmail.com>
2019-08-12 15:12:00 +0600

    дз

---
+++ Лабораторная 8. Базовые операции с массивами/Д.З. доп Зад. 8.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Д.З.доп_Зад._8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] data;
+            data = new String[] { "a", "b", "c", "d", "e", "f", "g", "h" };
+            String x = Console.ReadLine();
+            int a = 0;
+            int b = 0;
+            while (a < 8)
+            {
+                if (x == data[b])
+                {
+
+                    Console.Write(data[b]);
+                    Console.Write("1");
+
+
+                }
+                else
+                {
+                    Console.Write(data[b]);
+                    
+                }
+                
+
+                b++;
+                a++;
+            }
+        }
+    }
+}
