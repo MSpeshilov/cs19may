0d7572c254993c1aba8a84daa264282350b30949
MSpeshilov <slesar19966@gmail.com>
2019-09-03 20:21:03 +0600

    Задание 10. Лаб. 9

---
+++ Лабораторная 9. Использование числовых массивов/Задание 10. Использование числовых массивов/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_10.Использование_числовых_массивов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String dd = Console.ReadLine();
+            String[] data = dd.Split(' ');
+            int a = 0;
+            int b = data.Length - 1;
+            int c = 0;
+            int d = 1;
+            int e = 0;
+            int f = 0;
+            int[] num = new int[data.Length];
+            if (String.IsNullOrEmpty(dd))
+            {
+                Console.Write("Массив пуст");
+                return;
+            }
+            while (a< data.Length)
+            {
+                num[a] = int.Parse(data[a]);
+                a++;
+            }a = 0;
+            while (e < data.Length)
+            {
+                Console.Write("шаг {0} ", e);
+                
+
+                while (a < b)
+                {
+
+                    if (num[a] > num[d])
+                    {
+                        c = num[d];
+                        num[d] = num[a];
+                        num[a] = c;
+                        Console.Write(" Меняем {0} и {1}", num[a], num[d]);
+                    }
+                    else
+                    {
+                        Console.Write(" Оставляем {0} и {1}", num[a], num[d]);
+                    }
+                    
+                    d++;
+                    a++;
+
+                }
+                Console.WriteLine();
+                while (f < data.Length)
+                {
+                    Console.Write("{0}", num[f]);
+
+                    f++;
+                }
+                d = 1;
+                a = 0;
+                e++;
+                f = 0;
+            }
+            
+            
+           
+            
+            
+            
+        }
+    }
+}
